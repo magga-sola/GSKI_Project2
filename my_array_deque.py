0083c53b28b155cbb7d08379899f784c145efc43
Margret <margreta19@ru.is>
2020-02-16 20:44:31 +0000

    functions fyrir deque

--- my_array_deque.py
+++ my_array_deque.py
@@ -1,3 +1,26 @@
 
 class ArrayDeque():
-    pass
+    def __init__(self):
+        pass
+    def __str__(self):
+        """ Returns a string with all the items in the deque, separated by a single space """
+        pass
+
+    def push_back(self,value):
+        """ Takes a parameter and adds its value to the back of the deque """
+        pass
+    def push_front(self):
+        """ Takes a parameter and adds its value to the front of the deque """
+        pass
+    def pop_back(self):
+        """ Removes the item from the back of the deque and returns its value """
+        #if empty return None
+        pass
+    def pop_front(self):
+        """ Removes the item from the front of the deque and returns its value """
+        #if empty return None
+        pass
+    
+    def get_size(self):
+        """ Returns the number of items currently in the deque """
+        pass
