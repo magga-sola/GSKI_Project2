1aa8ca699822231dd104a37a09f79f74cdb8b85e
Margret <margreta19@ru.is>
2020-02-16 21:30:01 +0000

    linked shitttt

--- my_linked_list.py
+++ my_linked_list.py
@@ -1,3 +1,24 @@
 
 class LinkedList():
-    pass
+    """ A singly linked list """
+
+    def __init__(self,head=None,next=None):
+        self.head = head
+        self.next = next
+        
+    def __str__(self):
+        """ Returns a string with all the items in the list, separated by a single space """
+        pass
+
+    def push_back(self):
+        """ Takes a parameter and adds its value to the back of the list """
+        pass
+    def push_front(self):
+        """ Takes a parameter and adds its value to the front of the list """
+        pass
+    def pop_front(self):
+        """ Removes the item from the front of the list and ​returns its value """
+        pass
+    def get_size(self):
+        """ Returns the number of items currently in the list """
+        pass
