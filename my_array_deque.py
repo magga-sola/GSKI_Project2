734d7f1ed9d27127563a04cb6b7b692732afeeb3
EvaNaabye <evan19@ru.is>
2020-02-16 20:51:22 +0000

    init&str&resize

--- my_array_deque.py
+++ my_array_deque.py
@@ -1,26 +1,42 @@
 
 class ArrayDeque():
     def __init__(self):
-        pass
+        self.size = 0
+        self.capacity = 5
+        self.arr = [0] * self.capacity
+
     def __str__(self):
         """ Returns a string with all the items in the deque, separated by a single space """
-        pass
+        return_string = ""
+        for i in range(self.size):
+            return_string += str(self.arr[i]) + ", "
+        return return_string
+
+    def resize(self):
+        temp_list = [0] * (2*self.capacity)
+        for i in range(self.size):
+            temp_list[i] = self.arr[i]
+        self.arr = temp_list
+        self.capacity *= 2
 
     def push_back(self,value):
         """ Takes a parameter and adds its value to the back of the deque """
         pass
+
     def push_front(self):
         """ Takes a parameter and adds its value to the front of the deque """
         pass
+
     def pop_back(self):
         """ Removes the item from the back of the deque and returns its value """
         #if empty return None
         pass
+
     def pop_front(self):
         """ Removes the item from the front of the deque and returns its value """
         #if empty return None
         pass
-    
+
     def get_size(self):
         """ Returns the number of items currently in the deque """
         pass
