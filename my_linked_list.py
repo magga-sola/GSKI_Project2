e73fc2f798fda071dbb48433134a651a8fe2623a
Margret <margreta19@ru.is>
2020-02-17 12:50:28 +0000

    Update my_linked_list.py

--- my_linked_list.py
+++ my_linked_list.py
@@ -14,17 +14,41 @@ class LinkedList():
         
     def __str__(self):
         """ Returns a string with all the items in the list, separated by a single space """
-        pass
+        the_str = ""
+        if not self.isEmpty():
+            the_str += str(self._head)
+            self._head = self._next
+        return the_str
 
-    def push_back(self):
+    def isEmpty(self):
+        return self._size == 0
+
+    def push_back(self,value):
         """ Takes a parameter and adds its value to the back of the list """
-        pass
-    def push_front(self):
+        value_node = self.Node(value,None)
+        self._size += 1
+        if self.isEmpty():
+            self._head = value_node
+
+    def push_front(self,value):
         """ Takes a parameter and adds its value to the front of the list """
-        pass
+        if not self.isEmpty():
+            value_node = self.Node(value,self._head)
+            self._head = value_node
+            self._size += 1
+        else:
+            print("it's empty yo")
+
     def pop_front(self):
         """ Removes the item from the front of the list and ​returns its value """
-        pass
+        if not self.isEmpty():
+            #answer = self._head #answer
+            self._head = self._head._next
+            self._size -= 1
+            return self._head
+        else:
+            return "this is empty"
+
     def get_size(self):
         """ Returns the number of items currently in the list """
-        pass
+        return self._size
