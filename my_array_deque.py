adfe52c85e930a69f30e4a624fb40850137db26f
EvaNaabye <evan19@ru.is>
2020-02-17 15:08:39 +0000

    deque??

--- my_array_deque.py
+++ my_array_deque.py
@@ -4,6 +4,7 @@ class ArrayDeque():
         self.size = 0
         self.capacity = 5
         self.arr = [0] * self.capacity
+        self.start_index =  0
 
     def __str__(self):
         """ Returns a string with all the items in the deque, separated by a single space """
@@ -21,22 +22,60 @@ class ArrayDeque():
 
     def push_back(self,value):
         """ Takes a parameter and adds its value to the back of the deque """
-        pass
+        if self.size == self.capacity:
+            self.resize()
+
+        self.arr[self.size] = value
+        self.size += 1
 
     def push_front(self):
         """ Takes a parameter and adds its value to the front of the deque """
-        pass
+        if index > self.size or index < 0:
+            raise IndexOutOfBounds()
+        if index == 0:
+            return self.prepend(value)
+        if self.size == self.capacity:
+            self.resize()
+        for i in range(self.size, 0, -1):
+            if i > index:
+                self.arr[i] = self.arr[i - 1]
+            elif i == index:
+                self.arr[i] = value
+
+        self.size += 1
 
     def pop_back(self):
         """ Removes the item from the back of the deque and returns its value """
         #if empty return None
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.arr[self.size] = self.arr[self.size - 1]
+            self.size -= 1
 
     def pop_front(self):
         """ Removes the item from the front of the deque and returns its value """
         #if empty return None
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.arr[0] = self.arr[1]
+            self.size -= 1
 
     def get_size(self):
         """ Returns the number of items currently in the deque """
-        pass
+        return self.size
+
+deque = ArrayDeque()
+
+deque.push_back(4)
+print(deque)
+deque.push_back(5)
+print(deque)
+deque.push_back(9)
+print(deque)
+deque.pop_back()
+print(deque)
+deque.pop_front()
+print(deque)
+print(deque.get_size())
